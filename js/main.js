2a6e4456802b66bd6b29147298301dff55b06df6
Fabricio Ottavianelli <ottavianellifabricio@gmail.com>
2025-02-10 18:32:57 +1000

    todo listo falta emprolijar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Header
 
 document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.getElementById("menu-toggle");
+    const menuToggle = document.getElementById("menuhamburguesa");
     const menu = document.getElementById("menu");
 
     menuToggle.addEventListener("click", function () {
@@ -9,9 +9,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// Traer productos del json
 
 let totalCarrito = 0
+
 
 fetch ("./js/productos.json")
     .then(response => {
@@ -46,9 +46,22 @@ fetch ("./js/productos.json")
 
 
     function agregarproducto(producto) {
+
+        Toastify({
+            text: "Producto agregado, ingresa al carrito para completar la compra",
+            duration: 3000,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+            background: "linear-gradient(135deg, #ff6600, rgb(124, 106, 84))",
+            },
+            onClick: function(){} // Callback after click
+            }).showToast();
         let totalPrecio = document.getElementById("total")
         let itemCarrito = document.createElement("li")
-        itemCarrito.textContent = `${producto.nombre} - ${producto.precio}`
+        itemCarrito.textContent = `${producto.nombre} - $${producto.precio}`
         carrito.appendChild(itemCarrito)
     
 
@@ -59,6 +72,88 @@ fetch ("./js/productos.json")
 
     
     }
+
+    document.addEventListener("DOMContentLoaded", () => {
+
+        let totalCarrito = 0
+
+            // Elementos del carrito
+        let botonCarrito = document.getElementById("botoncarrito");
+        let carritoContainer = document.getElementById("carritocontainer");
+        let cerrarCarrito = document.getElementById("cerrarcarrito");
+
+        // Verificar si los elementos existen antes de usarlos
+        if (botonCarrito && carritoContainer && cerrarCarrito) {
+        // Mostrar el carrito
+        botonCarrito.addEventListener("click", () => {
+            carritoContainer.classList.add("active");
+        });
+
+        // Cerrar el carrito
+        cerrarCarrito.addEventListener("click", () => {
+            carritoContainer.classList.remove("active");
+        });
+        }
+    })
+
+    carrito.addEventListener ('click', (evento) => {
+
+        const ITEM = evento.target;
+
+        if (ITEM.tagName === 'LI') {
+            const PRECIOPRODUCTO = parseInt(ITEM.textContent.split('$')[1])
+            totalCarrito -= PRECIOPRODUCTO
+            const TOTAL_PRECIO = document.getElementById("total");
+            TOTAL_PRECIO.textContent = `Total: $${totalCarrito}`;
+            carrito.removeChild(ITEM)
+        }
+
+    })
+    
+    let checkoutButton = document.getElementById("checkout");
+
+    checkoutButton.addEventListener("click", () => {
+    Swal.fire({
+        title: "¿Seguro que deseas realizar esta compra?",
+        icon: "question",
+        showCancelButton: true, 
+        confirmButtonText: "Comprar",
+        cancelButtonText: "Cancelar",
+        customClass: {confirmButton: "swal-button-green" }
+    }).then((result) => {
+        if (result.isConfirmed) { 
+            
+            carrito.innerHTML = "";
+            totalCarrito = 0;
+            document.getElementById("total").textContent = `Total: $${totalCarrito}`;
+            
+            // Mostrar mensaje de éxito
+            Swal.fire({
+                title: "¡Compra realizada!",
+                icon: "success",
+                timer: 2000,
+                showConfirmButton: false
+            });
+        } else {
+            // Si cancela, no pasa nada
+            Swal.fire({
+                title: "Compra cancelada",
+                icon: "info",
+                timer: 2000,
+                showConfirmButton: false
+            });
+        }
+    })
+})
+
+
+
+
+
+
+   
+
+
 
 
 
